466711a9b5df988ffd4f06ef20782cccc05fa068
Mityo Draganov <mityodraganow@gmail.com>
2025-02-09 15:12:48 +0200

    shopping list page

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body
-                className={`${inter.className} h-screen flex flex-col overflow-hidden`}
+                className={`${inter.className} h-screen flex flex-col overflow-hidden bg-pink-50 `}
             >
                 <ClerkProvider
                     publishableKey={
